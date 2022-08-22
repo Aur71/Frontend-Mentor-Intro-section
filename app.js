72f46cf5a6e7b9f59d0a169930be05be511c28a3
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-08-22 15:31:35 +0300

    X

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const dropDown = document.querySelectorAll(".drop-down");
 const arrow = document.querySelectorAll("[data-arrow]");
 const text = document.querySelectorAll("[data-text]");
 
-for(let i = 0; i < dropBtn.length; i++) {
-    dropBtn[i].addEventListener("click", () => {
-        dropDown[i].classList.toggle("active");
-        text[i].classList.toggle("active-color")
-        
-        if(arrow[i].style.transform != "rotate(-180deg)") {
-            arrow[i].style.transform = "rotate(-180deg)";
-        } else {
-            arrow[i].style.transform = "rotate(0deg)";
-        }
-    })
+for (let i = 0; i < dropBtn.length; i++) {
+  dropBtn[i].addEventListener("click", () => {
+    dropDown[i].classList.toggle("active");
+    text[i].classList.toggle("active-color");
+
+    if (arrow[i].style.transform != "rotate(-180deg)") {
+      arrow[i].style.transform = "rotate(-180deg)";
+    } else {
+      arrow[i].style.transform = "rotate(0deg)";
+    }
+  });
 }
