387a61f38749c8bd108b028aba24a45c4b4081d9
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-08-23 11:30:52 +0300

    X

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const dropDown = document.querySelectorAll(".drop-down");
 const arrow = document.querySelectorAll("[data-arrow]");
 const text = document.querySelectorAll("[data-text]");
 const toggleBtn = document.querySelector("#toggleBtn");
-const header = document.querySelector("header")
-const bar = document.querySelectorAll(".bar")
+const header = document.querySelector("header");
+const bar = document.querySelectorAll(".bar");
 
 toggleBtn.addEventListener("click", () => {
   header.classList.toggle("active");
   bar[0].classList.toggle("btn-1-active");
   bar[2].classList.toggle("btn-2-active");
   bar[1].classList.toggle("active-bar");
-})
+});
 
 for (let i = 0; i < dropBtn.length; i++) {
   dropBtn[i].addEventListener("click", () => {
